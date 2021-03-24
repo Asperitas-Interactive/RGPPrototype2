2d047a3390b4750997cce8b4b1499cb26f6a58be
himchawla <himchawla43@gmail.com>
2021-03-25 10:23:39 +1300

    revamped cam movement

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,26 +6,27 @@ using UnityEngine.SceneManagement;
 public class playerMovement : MonoBehaviour
 {
 
-    public float mouseSensitivity;
-    float xRotation = 0f;
-    float yRotation = 0f;
+
+    float smoothTime = 0.1f;
+    float smoothVel;
 
     public bool glide;
 
     //Movement Based Variables
     public CharacterController controller;
-
-
     public float speed = 12.0f;
-
     public float gravity = -9.81f;
     public float jumpHeight;
+
+    public Vector3 external;
 
     Vector3 velocity;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+
+    private Transform cam;
 
     bool isGrounded;
     float defaultPos;
@@ -37,6 +38,7 @@ public class playerMovement : MonoBehaviour
     {
         //controller = gameObject.GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
+        cam = GameObject.FindGameObjectWithTag("MainCamera").transform;
     }
 
     // Update is called once per frame
@@ -44,15 +46,10 @@ public class playerMovement : MonoBehaviour
     {
 
 
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-        xRotation -= mouseY;
-        yRotation += mouseX;
-        Vector3 rot = (Vector3.up * xRotation);
+      
 
 
-        transform.localRotation = (Quaternion.Euler(0f, yRotation, 0f));
+      
 
 
         //Falling Down
@@ -70,10 +67,21 @@ public class playerMovement : MonoBehaviour
 
 
         //Move with local dir
-        Vector3 move = transform.right * x + transform.forward * y;
+        Vector3 move = new Vector3(x, 0f, y).normalized;
 
-        controller.Move(move * speed * Time.deltaTime);
 
+        float angle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+        float smAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref smoothVel, smoothTime);
+
+        transform.rotation = Quaternion.Euler(0f, smAngle, 0f);
+
+        if (move.magnitude > 0.1f || external.magnitude >0.1f)
+        {
+
+            Vector3 dir = Quaternion.Euler(0f, smAngle, 0f) * Vector3.forward;
+            controller.Move(dir * speed * Time.deltaTime + external * Time.deltaTime);
+        }
         velocity.y += gravity * Time.deltaTime;
 
         //Fall down
@@ -85,7 +93,7 @@ public class playerMovement : MonoBehaviour
         if(Input.GetButton("Glide"))
         {
             if (gravity < -1f)
-                gravity += 10f * Time.deltaTime;
+                gravity += 5f * Time.deltaTime;
             else
                 gravity = -1f;
         } 
